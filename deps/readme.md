f83ff15256ed6d21153d2bcd077c440f47b92de7
Mateus Orsoni <matorsoni45@gmail.com>
2024-09-10 21:23:43 -0300

    Add GLAD as dependency and create first window

--- deps/readme.md
+++ deps/readme.md
@@ -3,3 +3,7 @@ Removed the following content from the original GLFW source:
 - glfw/examples
 - glfw/tests
 
+GLAD2 - OpenGL Core 4.3
+Permalink:
+    - https://gen.glad.sh/#generator=c&api=gl%3D4.3&profile=gl%3Dcore%2Cgles1%3Dcommon
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,52 @@
 #include <cstdio>
+#include <cstdlib>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include "glad/gl.h"
 
 int main(void)
 {
-    printf("Hello dreams\n");
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        printf("Error: Failed to load OpenGL with GLAD.\n");
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(1.0, 0.5, 0.2, 1.0);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
